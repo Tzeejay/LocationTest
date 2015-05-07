0bcee522839265c82abb94836891193c8e9d58f9
Constantin Jacob <C.Jacob@live.de>
2015-05-07 18:59:04 +0200

    Initial commit

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -10,14 +10,36 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) CLLocationManager *manager;
+
 @end
 
 @implementation ViewController
+@synthesize label;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if (_manager == nil) {
+        _manager = [[CLLocationManager alloc] init];
+        [_manager requestAlwaysAuthorization];
+    }
+    
+    _manager.delegate = self;
+    _manager.desiredAccuracy = kCLLocationAccuracyBest;
+    [_manager startUpdatingLocation];
 }
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    for (CLLocation *location in locations) {
+        NSLog(@"%@", location.description);
+        label.text = location.description;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
