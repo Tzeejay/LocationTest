587f79b25b49b576ddf13e82d78235ba580eddab
Constantin Jacob <C.Jacob@live.de>
2015-05-07 13:41:23 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Constantin Jacob on 07.05.15.
+//  Copyright (c) 2015 Constantin Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Constantin Jacob on 07.05.15.
+//  Copyright (c) 2015 Constantin Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
